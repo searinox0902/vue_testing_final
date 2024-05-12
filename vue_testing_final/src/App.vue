e80972eac0b70a305b2d096f58fa45b7dd819bd7
svargasEsinergia <sebastian.vargas@esinergia.co>
2024-05-12 09:52:28 -0500

    Add vite-vue env, add login & register components

---
+++ vue_testing_final/src/App.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+  import Login from './components/login/login/login.vue'
+  import Register from './components/login/register/register.vue'
+</script>
+
+
+
+
+<template>
+  <Login v-if="false"></Login>
+  <Register v-if="true"></Register>
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
---
+++ vue_testing_final/src/components/login/register/register.vue
@@ -0,0 +1,105 @@
+<!-- 
+    /////////////
+        Registro
+    ////////////
+-->
+
+<script setup lang="ts">
+import { ref } from 'vue'
+
+defineProps<{ msg: string }>()
+const count = ref(0);
+</script>
+
+<template>
+    <div class="min-h-screen w-fill bg-gray-200 flex items-center justify-center">
+
+        <div class="px-12 py-10 bg-gray-300 rounded-xl space-y-4 hover:border-2 hover:border-blue-400">
+
+            <form class="space-y-4 ">
+                <div class="mb-6">
+                    <h1 class="text-2xl font-bold text-blue-800">Registro de cuentas</h1>
+                </div>
+
+                <!-- Name -->
+                <div>
+                    <label for="name" class="block text-sm font-medium leading-6 text-gray-900">Nombre de
+                        usuario</label>
+                    <div class="relative mt-2 rounded-md shadow-sm">
+                        <div class="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-2">
+                            <span class="text-gray-500 sm:text-sm">
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                    stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                                    <path stroke-linecap="round" stroke-linejoin="round"
+                                        d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
+                                </svg>
+                            </span>
+                        </div>
+                        <input type="text" required name="name" id="name" placeholder="Nombre"
+                            class="block w-full rounded-md border-0 py-1.5 pl-10 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
+                    </div>
+                </div>
+
+                <!-- Password -->
+                <div>
+                    <label for="name" required class="block text-sm font-medium leading-6 text-gray-900">Contraseña</label>
+                    <div class="relative mt-2 rounded-md shadow-sm">
+                        <div class="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-2">
+                            <span class="text-gray-500 sm:text-sm">
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                    stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                                    <path stroke-linecap="round" stroke-linejoin="round"
+                                        d="M16.5 10.5V6.75a4.5 4.5 0 1 0-9 0v3.75m-.75 11.25h10.5a2.25 2.25 0 0 0 2.25-2.25v-6.75a2.25 2.25 0 0 0-2.25-2.25H6.75a2.25 2.25 0 0 0-2.25 2.25v6.75a2.25 2.25 0 0 0 2.25 2.25Z" />
+                                </svg>
+                            </span>
+                        </div>
+                        <input type="password" name="name" id="name"
+                            class="block w-full rounded-md border-0 py-1.5 pl-10 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
+                    </div>
+                </div>
+
+
+                <!-- mail -->
+                <div>
+                    <label for="name" class="block text-sm font-medium leading-6 text-gray-900">Email</label>
+                    <div class="relative mt-2 rounded-md shadow-sm">
+                        <div class="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-2">
+                            <span class="text-gray-500 sm:text-sm">
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                    stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                                    <path stroke-linecap="round" stroke-linejoin="round"
+                                        d="M21.75 6.75v10.5a2.25 2.25 0 0 1-2.25 2.25h-15a2.25 2.25 0 0 1-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0 0 19.5 4.5h-15a2.25 2.25 0 0 0-2.25 2.25m19.5 0v.243a2.25 2.25 0 0 1-1.07 1.916l-7.5 4.615a2.25 2.25 0 0 1-2.36 0L3.32 8.91a2.25 2.25 0 0 1-1.07-1.916V6.75" />
+                                </svg>
+
+                            </span>
+                        </div>
+                        <input type="mail" required name="name" id="name" placeholder="Email"
+                            class="block w-full rounded-md border-0 py-1.5 pl-10 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
+                    </div>
+                </div>
+
+                <!-- submit -->
+                <div>
+                    <input type="submit"
+                        class="bg-blue-800 w-full py-1 rounded font-bold text-white mt-2  hover:cursor-pointer"
+                        value="Ingresar">
+                </div>
+            </form>
+
+
+            <!-- button -->
+            <div class="flex flex-col">
+                <a class="bg-white w-full py-1 rounded font-bold text-blue-800 mt-2 text-center  hover:cursor-pointer">
+                    Registrate</a>
+                <a
+                    class="w-full py-1 rounded text-black mt-2 text-center text-sm  hover:cursor-pointer hover:text-blue-800 hover:font-bold">
+                    Recuperar contraseña...</a>
+            </div>
+
+        </div>
+
+    </div>
+</template>
+
+
+<style scoped></style>
