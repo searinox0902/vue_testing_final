5ed878fc97bfe5547692523d14df50a16973282d
svargasEsinergia <sebastian.vargas@esinergia.co>
2024-05-12 21:19:40 -0500

    add base URL deploy github pages

--- vue_testing_final/vite.config.ts
+++ vue_testing_final/vite.config.ts
@@ -4,4 +4,5 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base : 'https://searinox0902.github.io/vue_testing_final/'
 })
